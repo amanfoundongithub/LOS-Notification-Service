b850a925051bcde9cdb2c3a974138e55a1fbdcb8
Aman Raj <billygates124356@gmail.com>
2026-05-31 00:29:37 +0530

    fixed service, mongodb up

--- src/main/java/com/loan_org/notification_service/config/RabbitMQConfig.java
+++ src/main/java/com/loan_org/notification_service/config/RabbitMQConfig.java
@@ -1,7 +1,7 @@
 package com.loan_org.notification_service.config;
 
 import org.springframework.amqp.core.*;
-import org.springframework.amqp.support.converter.JacksonJsonMessageConverter;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -96,6 +96,6 @@ public class RabbitMQConfig {
     // --- JSON Utility Transformer ---
     @Bean
     public MessageConverter jsonMessageConverter() {
-        return new JacksonJsonMessageConverter();
+        return new Jackson2JsonMessageConverter();
     }
 }
