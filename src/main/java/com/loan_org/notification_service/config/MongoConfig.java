8192b98d697b94a54aacf8bee6ae47ffec3aed17
Aman Raj <billygates124356@gmail.com>
2026-06-02 15:34:10 +0530

    enabled mongo auditing

--- src/main/java/com/loan_org/notification_service/config/MongoConfig.java
+++ src/main/java/com/loan_org/notification_service/config/MongoConfig.java
@@ -1,7 +1,10 @@
 package com.loan_org.notification_service.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.config.EnableMongoAuditing;
 
 @Configuration
+@EnableMongoAuditing
 public class MongoConfig {
+    // Mongo configuration enabled here
 }
