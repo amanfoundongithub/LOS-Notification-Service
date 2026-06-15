6b37013e4bb7eccd37e055705fba6827f0a9a1d0
Aman Raj <billygates124356@gmail.com>
2026-06-16 00:24:55 +0530

    fixed errors

--- src/main/java/com/loan_org/notification_service/shared/exception/channel/EmailDeliveryFailureException.java
+++ src/main/java/com/loan_org/notification_service/shared/exception/channel/EmailDeliveryFailureException.java
@@ -1,12 +1,14 @@
 package com.loan_org.notification_service.shared.exception.channel;
 
+import com.loan_org.notification_service.domain.dispatcher.channels.NotificationChannel;
 import com.loan_org.notification_service.shared.exception.NotificationDeliveryException;
-import com.loan_org.notification_service.shared.util.MaskingUtil;
+
+import static com.loan_org.notification_service.shared.util.MaskingUtil.maskRecipient;
 
 public class EmailDeliveryFailureException extends NotificationDeliveryException {
     public EmailDeliveryFailureException(String email, Throwable e) {
         super(
-                "Failed to deliver email to " + MaskingUtil.maskEmail(email) +
+                "Failed to deliver email to " + maskRecipient(email, NotificationChannel.EMAIL) +
                         ". Reason: " + e.getMessage()
         );
     }
