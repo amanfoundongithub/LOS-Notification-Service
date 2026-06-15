d51138f703cdfa49e2fa46e7300732d4ea03865b
Aman Raj <billygates124356@gmail.com>
2026-06-15 23:02:22 +0530

    updated log service

--- src/main/java/com/loan_org/notification_service/domain/audit/service/NotificationLogService.java
+++ src/main/java/com/loan_org/notification_service/domain/audit/service/NotificationLogService.java
@@ -1,11 +1,12 @@
-package com.loan_org.notification_service.domain.audit;
+package com.loan_org.notification_service.domain.audit.service;
 
+import com.loan_org.notification_service.domain.audit.entity.NotificationLogDocument;
 import com.loan_org.notification_service.dto.NotificationRequest;
 
 public interface NotificationLogService {
     NotificationLogDocument createRecord(NotificationRequest request);
     NotificationLogDocument getRecordById(String id);
     NotificationLogDocument updateRetryTelemetry(String id, int currentRetryCount, long upcomingDelayMs);
-    void updateStatusToSent(String id, String providerRefId);
-    public void updateStatusToFailed(String id, Exception exception);
+    void                    updateStatusToSent(String id, String providerRefId);
+    void                    updateStatusToFailed(String id, Exception exception);
 }
