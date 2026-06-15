88b79c942dcf69542fd786dc00f80f9f8f575689
Aman Raj <billygates124356@gmail.com>
2026-06-15 21:53:11 +0530

    removed un-needed spaces

--- src/main/java/com/loan_org/notification_service/config/properties/RabbitMQTopologyProperties.java
+++ src/main/java/com/loan_org/notification_service/config/properties/RabbitMQTopologyProperties.java
@@ -29,5 +29,4 @@ public class RabbitMQTopologyProperties {
     public static final String X_DEAD_LETTER_EXCHANGE = "x-dead-letter-exchange";
     public static final String X_DEAD_LETTER_ROUTING_KEY = "x-dead-letter-routing-key";
 
-
 }
