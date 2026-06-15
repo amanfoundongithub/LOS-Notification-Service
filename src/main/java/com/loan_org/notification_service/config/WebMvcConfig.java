cf34357f3894738005a2e8ef147f4cc01b60ca39
Aman Raj <billygates124356@gmail.com>
2026-06-15 22:48:39 +0530

    added path pattern & config

--- src/main/java/com/loan_org/notification_service/config/WebMvcConfig.java
+++ src/main/java/com/loan_org/notification_service/config/WebMvcConfig.java
@@ -1,6 +1,6 @@
 package com.loan_org.notification_service.config;
 
-import com.loan_org.notification_service.shared.util.MdcInterceptor;
+import com.loan_org.notification_service.config.interceptor.MdcInterceptor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
@@ -9,11 +9,13 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 @RequiredArgsConstructor
 public class WebMvcConfig implements WebMvcConfigurer {
+
     private final MdcInterceptor mdcInterceptor;
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(mdcInterceptor);
+        registry.addInterceptor(mdcInterceptor)
+                .addPathPatterns("/api/**");
     }
 
 }
