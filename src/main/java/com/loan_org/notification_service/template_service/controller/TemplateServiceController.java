e485cdeb59784cd5f5a4e686fa4b13ac1f801fec
Aman Raj <billygates124356@gmail.com>
2026-06-17 11:22:02 +0530

    added attribute for controller

--- src/main/java/com/loan_org/notification_service/template_service/controller/TemplateServiceController.java
+++ src/main/java/com/loan_org/notification_service/template_service/controller/TemplateServiceController.java
@@ -1,16 +1,16 @@
 package com.loan_org.notification_service.template_service.controller;
 
 import com.loan_org.notification_service.domain.template.entity.NotificationTemplateDocument;
+import com.loan_org.notification_service.template_service.access_control.TemplateServiceAccessControl;
 import com.loan_org.notification_service.template_service.dto.TemplateCreationRequest;
 import com.loan_org.notification_service.template_service.service.TemplateService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
 
 @RestController
 @RequiredArgsConstructor
@@ -18,11 +18,14 @@ import org.springframework.web.bind.annotation.RestController;
 public class TemplateServiceController {
 
     private final TemplateService templateService;
+    private final TemplateServiceAccessControl templateServiceAccessControl;
 
     @PostMapping("/create")
     public ResponseEntity<NotificationTemplateDocument> createTemplate(
-            @Valid @RequestBody TemplateCreationRequest request
+            @Valid @RequestBody TemplateCreationRequest request,
+            @RequestAttribute("attributes") Map<String, Object> attributes
     ) {
+        templateServiceAccessControl.canCreate(attributes);
         return ResponseEntity.status(HttpStatus.CREATED).body(templateService.createTemplate(request));
     }
 
