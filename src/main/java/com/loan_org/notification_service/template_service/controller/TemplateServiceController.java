71af67d92cf20d76b9e12be52a8f08246d2e0573
Aman Raj <billygates124356@gmail.com>
2026-06-17 11:44:37 +0530

    update and added endpoints

--- src/main/java/com/loan_org/notification_service/template_service/controller/TemplateServiceController.java
+++ src/main/java/com/loan_org/notification_service/template_service/controller/TemplateServiceController.java
@@ -3,6 +3,7 @@ package com.loan_org.notification_service.template_service.controller;
 import com.loan_org.notification_service.domain.template.entity.NotificationTemplateDocument;
 import com.loan_org.notification_service.template_service.access_control.TemplateServiceAccessControl;
 import com.loan_org.notification_service.template_service.dto.TemplateCreationRequest;
+import com.loan_org.notification_service.template_service.dto.TemplateUpdateRequest;
 import com.loan_org.notification_service.template_service.service.TemplateService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -20,7 +21,7 @@ public class TemplateServiceController {
     private final TemplateService templateService;
     private final TemplateServiceAccessControl templateServiceAccessControl;
 
-    @PostMapping("/create")
+    @PostMapping
     public ResponseEntity<NotificationTemplateDocument> createTemplate(
             @Valid @RequestBody TemplateCreationRequest request,
             @RequestAttribute("attributes") Map<String, Object> attributes
@@ -28,5 +29,35 @@ public class TemplateServiceController {
         templateServiceAccessControl.canCreate(attributes);
         return ResponseEntity.status(HttpStatus.CREATED).body(templateService.createTemplate(request));
     }
+
+    @PatchMapping
+    public ResponseEntity<NotificationTemplateDocument> updateTemplate(
+            @Valid @RequestBody TemplateUpdateRequest request,
+            @RequestAttribute("attributes") Map<String, Object> attributes
+    ) {
+        templateServiceAccessControl.canUpdate(attributes);
+        return ResponseEntity.status(HttpStatus.OK).body(templateService.updateTemplate(request));
+    }
+
+    @GetMapping
+    public ResponseEntity<NotificationTemplateDocument> getTemplate(
+            @RequestParam("templateCode") String templateCode,
+            @RequestAttribute("attributes") Map<String, Object> attributes
+    ) {
+        templateServiceAccessControl.canRead(attributes);
+        return ResponseEntity.status(HttpStatus.OK).body(templateService.getTemplate(templateCode));
+    }
+
+    @DeleteMapping
+    public ResponseEntity<Void>  deleteTemplate(
+            @RequestParam("templateCode") String templateCode,
+            @RequestAttribute("attributes") Map<String, Object> attributes
+    ) {
+        templateServiceAccessControl.canDelete(attributes);
+        templateService.deleteTemplate(templateCode);
+        return ResponseEntity.noContent().build();
+    }
+
+
 
 }
