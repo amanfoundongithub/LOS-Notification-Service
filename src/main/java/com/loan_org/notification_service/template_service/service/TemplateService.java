92363edcdd5c8adbb0044df52451be58d2d6490b
Aman Raj <billygates124356@gmail.com>
2026-06-17 11:43:56 +0530

    crud operations added

--- src/main/java/com/loan_org/notification_service/template_service/service/TemplateService.java
+++ src/main/java/com/loan_org/notification_service/template_service/service/TemplateService.java
@@ -2,7 +2,11 @@ package com.loan_org.notification_service.template_service.service;
 
 import com.loan_org.notification_service.domain.template.entity.NotificationTemplateDocument;
 import com.loan_org.notification_service.template_service.dto.TemplateCreationRequest;
+import com.loan_org.notification_service.template_service.dto.TemplateUpdateRequest;
 
 public interface TemplateService {
     NotificationTemplateDocument createTemplate(TemplateCreationRequest request);
+    NotificationTemplateDocument updateTemplate(TemplateUpdateRequest request);
+    NotificationTemplateDocument getTemplate(String templateCode);
+    void                         deleteTemplate(String templateCode);
 }
